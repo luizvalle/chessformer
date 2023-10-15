09bb876cecf8577baba058c92f3e58486f6fbf1b
Luiz do Valle <lld2131@columbia.edu>
2023-10-15 19:31:29 -0400

    Add progress bars to dowload script

--- helper_scripts/chess_iterators.py
+++ helper_scripts/chess_iterators.py
@@ -2,7 +2,18 @@ import chess.pgn
 import zstandard as zstd
 import requests
 import io
+import sys
 
+
+class ResponseRawWrapper(io.IOBase):
+    def __init__(self, response_raw):
+        self.response_raw = response_raw
+        self.num_bytes_read = 0
+
+    def read(self, size=-1):
+        data = self.response_raw.read(size)
+        self.num_bytes_read += sys.getsizeof(data)
+        return data
 
 class CompressedPgnHeaderIterator:
     def __init__(self, download_link):
@@ -10,7 +21,8 @@ class CompressedPgnHeaderIterator:
         # Stream the results so we do not load everything
         # into memory at once
         self.response = requests.get(url=download_link, stream=True)
-        reader = dctx.stream_reader(self.response.raw)
+        self.response_raw = ResponseRawWrapper(self.response.raw)
+        reader = dctx.stream_reader(self.response_raw)
         self.text_stream = io.TextIOWrapper(reader, encoding="utf-8")
 
     def __iter__(self):
@@ -24,3 +36,8 @@ class CompressedPgnHeaderIterator:
             self.response.close()
             raise StopIteration
 
+    def total_num_bytes_read(self):
+        return self.response_raw.num_bytes_read
+
+    def total_num_bytes(self):
+        return int(self.response.headers.get("Content-Length", 0))
--- helper_scripts/download_game_metadata.py
+++ helper_scripts/download_game_metadata.py
@@ -5,7 +5,7 @@ import multiprocess
 
 from chess_iterators import CompressedPgnHeaderIterator
 from gdrive import GDrive
-from tqdm import tqdm
+from tqdm.auto import tqdm
 from multiprocess import Pool
 
 
@@ -21,7 +21,8 @@ def file_name_from_link(download_link):
     return path
 
 
-def process_headers(download_link):
+def process_headers(arg):
+    i, download_link = arg
     column_types = {
             "Event": "category",
             "Result": "category",
@@ -32,20 +33,28 @@ def process_headers(download_link):
             }
     new_file_name = file_name_from_link(download_link)
     headers = CompressedPgnHeaderIterator(download_link)
-    try:
-        games_info = [{
-            "Event": header["Event"],
-            "Result": header["Result"],
-            "WhiteElo": int(header["WhiteElo"]) if header["WhiteElo"].isnumeric() else 0,
-            "BlackElo": int(header["BlackElo"]) if header["BlackElo"].isnumeric() else 0,
-            "TimeControl": header["TimeControl"],
-            "Termination": header["Termination"]}
-            for header in headers]
-        df = pd.DataFrame(data=games_info).astype(column_types)
-        gDrive = GDrive(CREDENTIALS_JSON)
-        gDrive.write_dataframe(df, PARENT_DIR_ID, new_file_name)
-    except Exception as e:
-        print(f"Error processing {new_file_name}: {e}")
+    games_info = list()
+    date = re.search(r"(\d{4}-\d{2}).pgn.zst", download_link).group(1).strip()
+    with tqdm(total=headers.total_num_bytes(), position=i + 1, unit='B', unit_scale=True, leave=False, desc=date, ncols=80) as pbar:
+        try:
+            for header in headers:
+                info = {
+                        "Event": header["Event"],
+                        "Result": header["Result"],
+                        "WhiteElo": int(header["WhiteElo"]) if header["WhiteElo"].isnumeric() else 0,
+                        "BlackElo": int(header["BlackElo"]) if header["BlackElo"].isnumeric() else 0,
+                        "TimeControl": header["TimeControl"],
+                        "Termination": header["Termination"]
+                        }
+                games_info.append(info)
+                update_value = headers.total_num_bytes_read() - pbar.n
+                update_value = update_value if pbar.n + update_value <= headers.total_num_bytes() else headers.total_num_bytes() - pbar.n
+                pbar.update(update_value)
+            df = pd.DataFrame(data=games_info).astype(column_types)
+            gDrive = GDrive(CREDENTIALS_JSON)
+            gDrive.write_dataframe(df, PARENT_DIR_ID, new_file_name)
+        except Exception as e:
+            print(e)
 
 
 def main():
@@ -55,19 +64,21 @@ def main():
         requests.get(DOWNLOAD_LIST).text.split('\n'),
         reverse=False)
 
-    print(f"Found {len(download_links)} files to download.")
-    print("Checking how many were already processed...")
+    print(f"Found {len(download_links)} files to download.", flush=True)
+    print("Checking how many were already processed...", flush=True)
 
     existing_files = gDrive.get_files(PARENT_DIR_ID)
     unprocessed_links = [download_link for download_link in download_links
             if f"{file_name_from_link(download_link)}.zstd" not in existing_files]
+    unprocessed_links = list(enumerate(unprocessed_links)) # Add index so that tqdm can place the progress bar
 
-    print(f"{len(download_links) - len(unprocessed_links)} files already processed.")
-    print(f"Processing remaining {len(unprocessed_links)} files...")
+    print(f"{len(download_links) - len(unprocessed_links)} files already processed.", flush=True)
+    print(f"Processing remaining {len(unprocessed_links)} files...", flush=True)
     print(f"Using {N_PROCESSES} processes.")
+
     chunk_size = 1
     with Pool(N_PROCESSES) as pool:
-        list(tqdm(pool.imap(process_headers, unprocessed_links, chunksize=chunk_size), total=len(unprocessed_links)))
+        list(tqdm(pool.imap_unordered(process_headers, unprocessed_links, chunksize=chunk_size), desc="Overall progress", total=len(unprocessed_links), position=0, ncols=80, leave=True))
 
 
 if __name__ == "__main__":
