f9f8fa0fecc7a6e80265c2cd18684bab74de2579
luizvalle <lf.lemos.valle@gmail.com>
2023-11-15 19:37:35 -0500

    Add parameters to save model

--- model/setup.py
+++ model/setup.py
@@ -1,14 +1,11 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES = []
-
 setup(
     name='trainer',
     version='0.1',
-    install_requires=REQUIRED_PACKAGES,
     packages=find_packages(),
+    install_requires=(),
     include_package_data=True,
     description='My training application.'
 )
-
--- model/trainer/train.py
+++ model/trainer/train.py
@@ -9,17 +9,6 @@ from data import Dataset
 from model import ChessformerResultClassifier
 
 
-# Dataset parameters
-BUFFER_SIZE = 20000
-BATCH_SIZE = 128
-
-# Model parameters
-NUM_ENCODER_LAYERS = 6
-EMBEDDING_DIM = 512
-NUM_ATTENTION_HEADS = 8
-FEED_FORWARD_DIMENSION = 2048
-DROPOUT_RATE = 0.1
-
 # Training loop parameters
 SUM_OVER_BATCH_SIZE = tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE
 
@@ -37,25 +26,9 @@ class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
         return tf.math.rsqrt(self.d_k) * tf.math.minimum(arg1, arg2)
 
 
-@tf.function
-def train_step(moves, true_results):
-    with tf.GradientTape() as tape:
-        predicted_results = model(moves, training=True)
-        loss_value = loss_fn(true_results, predicted_results)
-    grads = tape.gradient(loss_value, model.trainable_weights)
-    optimizer.apply_gradients(zip(grads, model.trainable_weights))
-    acc_metric.update_state(true_results, predicted_results)
-    return loss_value
-
-
-@tf.function
-def val_step(moves, true_results):
-    predicted_results = model(moves, training=False)
-    acc_metric.update_state(true_results, predicted_results)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+def parse_args():
+    parser = argparse.ArgumentParser(
+            description="Keras Chess Game Result Predictor")
     parser.add_argument("--training_data_dir", dest="training_data_dir",
                         required=True, type=str,
                         help="Directory with tfrecord files.")
@@ -63,13 +36,19 @@ if __name__ == "__main__":
                         default=0.8, type=float,
                         help="The proportion of the overall dataset size to use for training.")
     parser.add_argument("--batch_size", dest="batch_size",
-                        default=128, type=int,
+                        default=64, type=int,
                         help="The batch size to use in training.")
     parser.add_argument("--shuffle_buffer_size", dest="shuffle_buffer_size",
                         default=20000, type=int,
                         help="The size of the buffer to use when shuffling the dataset.")
+    parser.add_argument("--model_save_dir", dest="model_save_dir",
+                        default=os.getenv("AIP_MODEL_DIR"), type=str,
+                        help="The location to save the final trained model.")
+    parser.add_argument("--model_checkpoint_dir", dest="model_checkpoint_dir",
+                        default=os.getenv("AIP_MODEL_DIR"), type=str,
+                        help="The location to save the model checkpoints.")
     parser.add_argument("--epochs", dest="epochs",
-                        default=10, type=int,
+                        default=25, type=int,
                         help="Number of epochs.")
     parser.add_argument("--num_encoder_layers", dest="num_encoder_layers",
                         default=6, type=int,
@@ -90,6 +69,28 @@ if __name__ == "__main__":
                         default=64, type=int,
                         help="The size of the hidden layer in the classification head feed-forward network.")
     args = parser.parse_args()
+    return args
+
+
+@tf.function
+def train_step(moves, true_results, model, loss_fn, optimizer, acc_metric):
+    with tf.GradientTape() as tape:
+        predicted_results = model(moves, training=True)
+        loss_value = loss_fn(true_results, predicted_results)
+    grads = tape.gradient(loss_value, model.trainable_weights)
+    optimizer.apply_gradients(zip(grads, model.trainable_weights))
+    acc_metric.update_state(true_results, predicted_results)
+    return loss_value
+
+
+@tf.function
+def val_step(moves, true_results, model, acc_metric):
+    predicted_results = model(moves, training=False)
+    acc_metric.update_state(true_results, predicted_results)
+
+
+def main():
+    args = parse_args()
 
     print("PARAMETERS:")
     for arg in vars(args):
@@ -110,8 +111,7 @@ if __name__ == "__main__":
             num_heads=args.num_attention_heads,
             encoder_dff=args.encoder_feed_forward_dim,
             classifier_dff=args.head_feed_forward_dim,
-            dropout_rate=args.dropout_rate
-            )
+            dropout_rate=args.dropout_rate)
 
     learning_rate = CustomSchedule(args.embedding_dim)
     optimizer = tf.keras.optimizers.Adam(
@@ -128,14 +128,14 @@ if __name__ == "__main__":
 
         # Iterate over the batches of the dataset.
         for step, (moves, true_elos, true_results) in enumerate(train_dataset):
-            loss_value = train_step(moves, true_results)
+            loss_value = train_step(
+                    moves, true_results, model, loss_fn, optimizer, acc_metric)
 
-            # TODO: Change this
-            # Log every 1 batch.
-            if step % 1 == 0:
+            # Log every 200 batches.
+            if step % 200 == 0:
                 print(
                     f"Training loss (for one batch) at step {step}: {float(loss_value):.4f}")
-                print(f"Seen so far: {(step + 1) * BATCH_SIZE} samples")
+                print(f"Seen so far: {(step + 1) * args.batch_size} samples")
 
         # Display metrics at the end of each epoch.
         accuracy = acc_metric.result()
@@ -146,10 +146,13 @@ if __name__ == "__main__":
 
         # Run a validation loop at the end of each epoch.
         for moves, true_elos, true_results in val_dataset:
-            val_step(moves, true_results)
+            val_step(moves, true_results, model, acc_metric)
 
         accuracy = acc_metric.result()
         print(f"Validation accuracy over epoch: {accuracy:.4f}")
-        val_elo_error_metric.reset_states()
-        val_result_acc_metric.reset_states()
+        acc_metric.reset_states()
         print(f"Time taken: {time.time() - start_time:.2f}s")
+
+
+if __name__ == "__main__":
+    main()
